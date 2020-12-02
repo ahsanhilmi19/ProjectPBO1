19db8400972e8ea6a72c26a4af123769977c14ab
Ahsan Hilmi <71971980+ahsanhilmi19@users.noreply.github.com>
2020-12-02 10:40:21 +0700

    Create project_class.py

---
+++ project_class.py
@@ -0,0 +1,13 @@
+class daftarMenu:
+
+    def __init__(self, ayamBakar, ayamGoreng, gurameAsam, ikanGurame, lalapan, nasput, nilaGoreng, sambel, tahu, tempe):
+        self.ayamBakar = ayamBakar
+        self.ayamGoreng = ayamGoreng
+        self.gurameAsam = gurameAsam
+        self.ikanGurame = ikanGurame
+        self.lalapan = lalapan
+        self.nasput = nasput
+        self.nilaGoreng = nilaGoreng
+        self.sambel = sambel
+        self.tahu = tahu
+        self.tempe = tempe
