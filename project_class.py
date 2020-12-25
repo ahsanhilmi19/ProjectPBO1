33b1f874d877095d29c22733dd58821e7e01b657
Ahsan Hilmi <71971980+ahsanhilmi19@users.noreply.github.com>
2020-12-25 14:27:57 +0700

    test

--- project_class.py
+++ project_class.py
@@ -1,13 +1,25 @@
-class daftarMenu:
+#parentclass
+class Customer():
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
 
-    def __init__(self, ayamBakar, ayamGoreng, gurameAsam, ikanGurame, lalapan, nasput, nilaGoreng, sambel, tahu, tempe):
-        self.ayamBakar = ayamBakar
-        self.ayamGoreng = ayamGoreng
-        self.gurameAsam = gurameAsam
-        self.ikanGurame = ikanGurame
-        self.lalapan = lalapan
-        self.nasput = nasput
-        self.nilaGoreng = nilaGoreng
-        self.sambel = sambel
-        self.tahu = tahu
-        self.tempe = tempe
+    def show(self):
+        print("Name :", self.name)
+        print("Number :", self.number)
+
+#chidclass
+class Dapur():
+    def __init__(self, name, number):
+        super().__init__(name, number)
+        self.balance = 0
+    
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Total :", self.balance)
+
+    #def withdraw(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print 
